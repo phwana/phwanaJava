76e6f5e3812974b3142ecb0ddc095f3fbc204a6f
phwana <roqkfstudy@gmail.com>
2024-12-20 14:16:25 +0900

    [ADD] Day18 스레드 개념 및 예제
    1. 멀티태스킹과 밀접한 관련이 있는 스레드
    
    2. 자바에서는 멀티스레드를 지원. 동시에 여러가지 기능을 수행하는 프로그램을 만들 수 있게 함.
    
    3. 스레드 작성 방법 2가지는 Thread 클래스 상속과 Runnable 인터페이스 상속하는 방법이 있음.
    인터페이스를 상속하는 방법을 선호하며 확장성 측면(다중 구현)에서 장점이 있음

---
+++ src/com/phwana/day15/stream/exercise/Exercise_ImageCopy.java
@@ -0,0 +1,14 @@
+package com.phwana.day15.stream.exercise;
+
+public class Exercise_ImageCopy {
+	/*
+	 * C:\Windows\Web\Wallpaper\Windows\img0.jpg를 복사해서
+	 * C:\Temp\copyimg.jpg로 만들어주세요
+	 * 단, 바이트기반 스트림을 사용하세요
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/com/phwana/day15/stream/exercise/Exercise_MemoPad.java
+++ src/com/phwana/day15/stream/exercise/Exercise_MemoPad.java
@@ -1,5 +1,9 @@
 package com.phwana.day15.stream.exercise;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class Exercise_MemoPad {
 	/*		
 	 *  // 저장할 파일 입력(확장자 제외) : snow
@@ -13,6 +17,12 @@ public class Exercise_MemoPad {
 		// 파일 저장이 완료되었습니다. F5를 눌러 확인해보세요~
 	 */
 	public static void main(String[] args) {
+		try {
+			Writer wr = new FileWriter("");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
